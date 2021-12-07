d415a8f9d9562bc4be788be43e69d0771bf6bde0
Oleksandr Zakandaiev <45367051+zakandaiev@users.noreply.github.com>
2021-12-07 16:29:59 +0200

    v1.0.0

--- gulpfile.js/tasks/clear.js
+++ gulpfile.js/tasks/clear.js
@@ -1,7 +1,7 @@
 const del = require("del");
 
 const clear = () => {
-	return del($.path.clear);
+	return del($.path.clear, {force: true});
 }
 
 module.exports = clear;
--- gulpfile.js/tasks/rootFiles.js
+++ gulpfile.js/tasks/rootFiles.js
@@ -1,5 +1,5 @@
 const rootFiles = () => {
-	return $.gulp.src($.path.rootFiles.src)
+	return $.gulp.src($.path.rootFiles.src, {dot: true})
 	.pipe($.plugins.newer($.path.rootFiles.dest))
 	.pipe($.gulp.dest($.path.rootFiles.dest))
 	.pipe($.browserSync.stream());
